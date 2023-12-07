d54f0a23c9696ff90139a2fae5801305d71a320e
PabloAndresCQ <pablo.andres-martinez@cambridgequantum.com>
2023-12-07 12:03:49 +0000

    Bug fix: TKET convention of angle parameters not taken into account

--- quimb_mps.py
+++ quimb_mps.py
@@ -1,5 +1,6 @@
 from typing import NamedTuple
 from pytket.circuit import Circuit, OpType
+import math
 
 from quimb.tensor.circuit import CircuitMPS
 
@@ -11,18 +12,21 @@ class Config(NamedTuple):
 def simulate(circ: Circuit, config: Config) -> CircuitMPS:
     """Simulate the circuit via MPS using Quimb on numpy backend.
     """
+
+    # NOTE: Gates with parameters need to have math.pi multiplied to them, due to TKET's angle convention
+
     mps = CircuitMPS(circ.n_qubits)
     for gate in circ.get_commands():
         if gate.op.type == OpType.H:
             mps.h(gate.qubits[0].index[0])
         elif gate.op.type == OpType.Rx:
-            mps.rx(gate.op.params[0], gate.qubits[0].index[0])
+            mps.rx(math.pi*gate.op.params[0], gate.qubits[0].index[0])
         elif gate.op.type == OpType.Rz:
-            mps.rz(gate.op.params[0], gate.qubits[0].index[0])
+            mps.rz(math.pi*gate.op.params[0], gate.qubits[0].index[0])
         elif gate.op.type == OpType.ZZPhase:
-            mps.rzz(gate.op.params[0], gate.qubits[0].index[0], gate.qubits[1].index[0])
+            mps.rzz(math.pi*gate.op.params[0], gate.qubits[0].index[0], gate.qubits[1].index[0])
         elif gate.op.type == OpType.XXPhase:
-            mps.rxx(gate.op.params[0], gate.qubits[0].index[0], gate.qubits[1].index[0])
+            mps.rxx(math.pi*gate.op.params[0], gate.qubits[0].index[0], gate.qubits[1].index[0])
         else:
             raise Exception(f"Unknown gate {gate.op.type}")
 
