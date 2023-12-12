b692c184b2ca600e8bd173212fe035d08869dafc
PabloAndresCQ <pablo.andres-martinez@cambridgequantum.com>
2023-12-12 16:15:41 +0000

    Added source code for Julia package and installation instructions in README

---
+++ KernelPkg/src/KernelPkg.jl
@@ -0,0 +1,66 @@
+module KernelPkg
+
+using ITensors
+
+export compute_tile
+
+# Gate definitions, since TKET's are slightly different
+function ITensors.op(::OpName"TKET_XXPhase", t::SiteType"Qubit"; α::Number)
+  θ = π*α/2
+  return [
+    cos(θ) 0 0 -im*sin(θ)
+    0 cos(θ) -im*sin(θ) 0
+    0 -im*sin(θ) cos(θ) 0
+    -im*sin(θ) 0 0 cos(θ)
+  ]
+end
+
+function ITensors.op(::OpName"TKET_Rz", t::SiteType"Qubit"; α::Number)
+  θ = π*α/2
+  return [
+    exp(-im * θ) 0
+    0 exp(im * θ)
+  ]
+end
+
+# Build and simulate the given circuit (as a list of gates)
+function build_and_sim_circ(circuit, site_inds, value_of_zero::Float64)
+  gates::Vector{ITensor} = []
+
+  for (name, qubits, params) in circuit
+    if name == "H"
+      append!(gates, [op("H", site_inds, 1+qubits[1])])
+    elseif name == "Rz"
+      append!(gates, [op("TKET_Rz", site_inds, 1+qubits[1]; α=params[1])])
+    elseif name == "XXPhase"
+      append!(gates, [op("TKET_XXPhase", site_inds, 1+qubits[1], 1+qubits[2]; α=params[1])])
+    else
+      error("KernelPkg error: Unrecognised gate.")
+    end
+  end
+
+  # Simulate the circuit
+  ψ = apply(gates, MPS(site_inds, "0"); cutoff=value_of_zero)
+
+  return ψ
+end
+
+# Compute all of the entries of a tile; includes MPS simulation of the circuits
+function compute_tile(n_qubits::Int64, x_circs, y_circs, value_of_zero::Float64)
+  tile = zeros(size(y_circs, 1), size(x_circs, 1))
+  site_inds = siteinds("Qubit", n_qubits)
+
+  # Generate all MPS
+  x_mps = [build_and_sim_circ(circ, site_inds, value_of_zero) for circ in eachrow(x_circs)]
+  y_mps = [build_and_sim_circ(circ, site_inds, value_of_zero) for circ in eachrow(y_circs)]
+
+  # For each pair, compute the inner product
+  pairs = [(i, j) for i in 1:length(y_mps) for j in 1:length(x_mps)]
+  for (i, j) in pairs
+    tile[i, j] = abs(inner(y_mps[i], x_mps[j]))^2
+  end
+
+  return tile
+end
+
+end  #module
--- README.md
+++ README.md
@@ -3,3 +3,28 @@
 Code for our experiments on Quantum Kernel methods being simulated on NVIDIA GPUs via pytket-cutensornet.
 
 The Elliptic Bitcoin Dataset needs to be downloaded and stored in the folder ``elliptic_bitcoin_dataset``. The two files should be ``elliptic_txs_classes.csv`` and ``elliptic_txs_features.csv``.
+
+### Installation
+
+Apart from Python and pytket, this branch makes use of Julia, ITensors.jl, pyCall and a local package KernelPkg.jl.
+
+To install *Julia*, follow: https://docs.julialang.org/en/v1/manual/getting-started/
+
+To install *ITensors.jl*, enter the Julia REPL and press `]` to enter the package manager. Type `add ITensors`.
+
+To install *pyCall*:
+- `pip install julia`
+- In Python REPL, `import julia` followed by `julia.install()`
+
+Test pyCall installation was successful by entering the Python REPL and typing:
+ ```
+from julia.api import Julia
+jl = Julia(compiled_modules=False)
+from julia import Base
+Base.sind(60)
+ ```
+
+Finally, to install *KernelPkg.jl*, move the contents of the folder `KernelPkg` in this repository to `~/.julia/dev/KernelPkg`. Then, open the Julia REPL and press `]` to enter the package manager. Locally install the package by typing:
+`dev ~/.julia/dev/KernelPkg`.
+
+**NOTE**: This branch uses MPI, so you will also need to install `mpi4py` (e.g. via pip) and execute the python script accordingly (e.g. via `mpirun` or `mpiexec`).
